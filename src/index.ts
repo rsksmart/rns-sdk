94f8d9ba802dc9797719ab4994d27f50600e5141
Ilan <ilanolkies@outlook.com>
2021-11-28 22:50:27 -0300

    Refactor addr resolver abi

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { Signer, Contract } from 'ethers'
 import { hash as namehash } from '@ensdomains/eth-ens-namehash'
 import { keccak_256 as sha3 } from 'js-sha3'
 import RNSRegistryData from '@rsksmart/rns-registry/RNSRegistryData.json'
-import AddrResolverData from '@rsksmart/rns-resolver/AddrResolverData.json'
 
 export const hashDomain = (domain: string) => namehash(domain)
 export const hashLabel = (label: string) => '0x' + sha3(label)
+
+const addrResolverAbi = [
+  'function addr(bytes32 node) public view returns (address)',
+  'function setAddr(bytes32 node, address addr) public'
+]
 
 export class RNS {
   rnsRegistry: Contract
@@ -16,9 +20,9 @@ export class RNS {
     this.signer = signer
   }
 
-  private async getResolverContract (domainHash: string) {
+  private async getAddrResolverContract (domainHash: string) {
     const resolverAddress = await this.rnsRegistry.resolver(domainHash)
-    return new Contract(resolverAddress, AddrResolverData.abi).connect(this.signer)
+    return new Contract(resolverAddress, addrResolverAbi).connect(this.signer)
   }
 
   setSubnodeOwner (domain: string, label: string, owner: string) {
@@ -30,14 +34,14 @@ export class RNS {
 
   async setAddr (domain: string, addr: string) {
     const domainHash = hashDomain(domain)
-    const resolverContract = await this.getResolverContract(domainHash)
+    const resolverContract = await this.getAddrResolverContract(domainHash)
 
     return resolverContract.setAddr(domainHash, addr)
   }
 
   async addr (domain: string) {
     const domainHash = hashDomain(domain)
-    const resolverContract = await this.getResolverContract(domainHash)
+    const resolverContract = await this.getAddrResolverContract(domainHash)
 
     return resolverContract.addr(domainHash)
   }
